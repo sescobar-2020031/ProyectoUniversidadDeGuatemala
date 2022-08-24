ed2c4dc800f7390f6e7424dc7c311ed4c60d2dad
sescobar-2020031 <sescobar-2020031@kinal.edu.gt>
2022-08-24 14:09:25 -0600

    Se agregaron funciones para la tabla - Backend

--- ProyectoUniversidadDeGuatemala_BackEnd/src/controllers/contestant.controller.js
+++ ProyectoUniversidadDeGuatemala_BackEnd/src/controllers/contestant.controller.js
@@ -96,7 +96,7 @@ exports.saveContestant = async (req, res) => {
             data.declamationDate = new Date(currentDate).setDate(dayToDeclaim);
         } else {
             if (currentWeekDay == 0) dayToDeclaim = currentDay + 5;
-            else if (currentWeekDay == 1) dayToDeclaim = currentDay + 4; 
+            else if (currentWeekDay == 1) dayToDeclaim = currentDay + 4;
             else if (currentWeekDay == 2) dayToDeclaim = currentDay + 3;
             else if (currentWeekDay == 3) dayToDeclaim = currentDay + 2;
             else if (currentWeekDay == 4) dayToDeclaim = currentDay + 1;
@@ -120,9 +120,209 @@ exports.getContestants = async (req, res) => {
         const participants = await Contestant.find();
         if (participants.length === 0) return res.status(400).send({ message: 'No se encontraron participantes' });
         return res.send({ message: 'Participantes encontrados: ', participants });
-    } catch (er) {
+    } catch (err) {
         console.log(err);
         return res.status(500).send({ err, message: 'Error obteniendo participantes' });
+    }
+}
+
+exports.getContestantsCareerAtoZ = async (req, res) => {
+    try {
+        const participantsCareerAtoZ = await Contestant.find();
+        if (participantsCareerAtoZ.length === 0) return res.status(400).send({ message: 'No se encontraron participantes' });
+        participantsCareerAtoZ.sort((a, b) => {
+            if (a.studentCareer < b.studentCareer) {
+                return -1;
+            } else if (b.studentCareer > a.studentCareer) {
+                return 1;
+            } else {
+                return 0
+            }
+        })
+        return res.send({ message: 'Participantes encontrados: ', participantsCareerAtoZ });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ err, message: 'Error obteniendo participantes' });
+    }
+}
+
+exports.getContestantsCareerZtoA = async (req, res) => {
+    try {
+        const participantsCareerZtoA = await Contestant.find();
+        if (participantsCareerZtoA.length === 0) return res.status(400).send({ message: 'No se encontraron participantes' });
+        participantsCareerZtoA.sort((a, b) => {
+            if (a.studentCareer > b.studentCareer) {
+                return -1;
+            } else if (b.studentCareer < a.studentCareer) {
+                return 1;
+            } else {
+                return 0
+            }
+        })
+        return res.send({ message: 'Participantes encontrados: ', participantsCareerZtoA });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ err, message: 'Error obteniendo participantes' });
+    }
+}
+
+exports.getContestantsAgeUpward = async (req, res) => {
+    try {
+        const participantsAgeUpward = await Contestant.find();
+        if (participantsAgeUpward === 0) return res.status(400).send({ message: 'No se encontraron participantes' });
+        participantsAgeUpward.sort((a, b) => { return a.age - b.age })
+        return res.send({ message: 'Participantes encontrados: ', participantsAgeUpward });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ err, message: 'Error obteniendo participantes' });
+    }
+}
+
+exports.getContestantsAgeDescendant = async (req, res) => {
+    try {
+        const participantsAgeDescendant = await Contestant.find();
+        if (participantsAgeDescendant === 0) return res.status(400).send({ message: 'No se encontraron participantes' });
+        participantsAgeDescendant.sort((a, b) => { return b.age - a.age })
+        return res.send({ message: 'Participantes encontrados: ', participantsAgeDescendant });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ err, message: 'Error obteniendo participantes' });
+    }
+}
+
+exports.getContestantsPoetryGenreAtoZ = async (req, res) => {
+    try {
+        const participantsPoetryGenreAtoZ = await Contestant.find();
+        if (participantsPoetryGenreAtoZ === 0) return res.status(400).send({ message: 'No se encontraron participantes' });
+        participantsPoetryGenreAtoZ.sort((a, b) => {
+            if (a.poetryGenre < b.poetryGenre) {
+                return -1;
+            } else if (b.poetryGenre > a.poetryGenre) {
+                return 1;
+            } else {
+                return 0
+            }
+        })
+        return res.send({ message: 'Participantes encontrados: ', participantsPoetryGenreAtoZ });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ err, message: 'Error obteniendo participantes' });
+    }
+}
+
+exports.getContestantsPoetryGenreZtoA = async (req, res) => {
+    try {
+        const participantsPoetryGenreZtoA = await Contestant.find();
+        if (participantsPoetryGenreZtoA.length === 0) return res.status(400).send({ message: 'No se encontraron participantes' });
+        participantsPoetryGenreZtoA.sort((a, b) => {
+            if (a.poetryGenre > b.poetryGenre) {
+                return -1;
+            } else if (b.poetryGenre < a.poetryGenre) {
+                return 1;
+            } else {
+                return 0
+            }
+        })
+        return res.send({ message: 'Participantes encontrados: ', participantsPoetryGenreZtoA });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ err, message: 'Error obteniendo participantes' });
+    }
+}
+
+exports.getContestantsPoetryGenreAtoZ = async (req, res) => {
+    try {
+        const participantsPoetryGenreAtoZ = await Contestant.find();
+        if (participantsPoetryGenreAtoZ === 0) return res.status(400).send({ message: 'No se encontraron participantes' });
+        participantsPoetryGenreAtoZ.sort((a, b) => {
+            if (a.poetryGenre < b.poetryGenre) {
+                return -1;
+            } else if (b.poetryGenre > a.poetryGenre) {
+                return 1;
+            } else {
+                return 0
+            }
+        })
+        return res.send({ message: 'Participantes encontrados: ', participantsPoetryGenreAtoZ });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ err, message: 'Error obteniendo participantes' });
+    }
+}
+
+exports.getContestantsDeclamationDateUpward = async (req, res) => {
+    try {
+        const participantsDeclamationDateUpward = await Contestant.find();
+        if (participantsDeclamationDateUpward.length === 0) return res.status(400).send({ message: 'No se encontraron participantes' });
+        participantsDeclamationDateUpward.sort((a, b) => {
+            const firstDate = new Date(a.declamationDate);
+            const secondDate = new Date(b.declamationDate);
+            if (firstDate < secondDate) return -1;
+            if (firstDate > secondDate) return 1;
+            return 0;
+        })
+        return res.send({ message: 'Participantes encontrados: ', participantsDeclamationDateUpward });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ err, message: 'Error obteniendo participantes' });
+    }
+}
+
+exports.getContestantsDeclamationDateDescendant = async (req, res) => {
+    try {
+        const participantsDeclamationDateDescendant = await Contestant.find();
+        if (participantsDeclamationDateDescendant.length === 0) return res.status(400).send({ message: 'No se encontraron participantes' });
+        participantsDeclamationDateDescendant.sort((a, b) => {
+            const firstDate = new Date(a.declamationDate);
+            const secondDate = new Date(b.declamationDate);
+            if (firstDate > secondDate) return -1;
+            if (firstDate < secondDate) return 1;
+            return 0;
+        })
+        return res.send({ message: 'Participantes encontrados: ', participantsDeclamationDateDescendant });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ err, message: 'Error obteniendo participantes' });
+    }
+}
+
+exports.getContestantsFullNameAtoZ = async (req, res) => {
+    try {
+        const participantsFullNameAtoZ = await Contestant.find();
+        if (participantsFullNameAtoZ.length === 0) return res.status(400).send({ message: 'No se encontraron participantes' });
+        participantsFullNameAtoZ.sort((a, b) => {
+            if (a.fullName < b.fullName) {
+                return -1;
+            } else if (b.fullName > a.fullName) {
+                return 1;
+            } else {
+                return 0
+            }
+        })
+        return res.send({ message: 'Participantes encontrados: ', participantsFullNameAtoZ });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ err, message: 'Error obteniendo participantes' })
+    }
+}
+
+exports.getContestantsFullNameZtoA = async (req, res) => {
+    try {
+        const participantsFullNameZtoA = await Contestant.find();
+        if (participantsFullNameZtoA.length === 0) return res.status(400).send({ message: 'No se encontraron participantes' });
+        participantsFullNameZtoA.sort((a, b) => {
+            if (a.fullName > b.fullName) {
+                return -1;
+            } else if (b.fullName < a.fullName) {
+                return 1;
+            } else {
+                return 0
+            }
+        })
+        return res.send({ message: 'Participantes encontrados: ', participantsFullNameZtoA });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ err, message: 'Error obteniendo participantes' })
     }
 }
 
--- ProyectoUniversidadDeGuatemala_BackEnd/src/routes/contestant.routes.js
+++ ProyectoUniversidadDeGuatemala_BackEnd/src/routes/contestant.routes.js
@@ -8,6 +8,16 @@ const contestantController = require('../controllers/contestant.controller');
 api.get('/testContestant', contestantController.testContestant);
 api.post('/saveContestant', contestantController.saveContestant);
 api.get('/getContestants', contestantController.getContestants);
+api.get('/getContestantsCareerAtoZ', contestantController.getContestantsCareerAtoZ);
+api.get('/getContestantsCareerZtoA', contestantController.getContestantsCareerZtoA);
+api.get('/getContestantsAgeUpward', contestantController.getContestantsAgeUpward);
+api.get('/getContestantsAgeDescendant', contestantController.getContestantsAgeDescendant);
+api.get('/getContestantsPoetryGenreAtoZ', contestantController.getContestantsPoetryGenreAtoZ);
+api.get('/getContestantsPoetryGenreZtoA', contestantController.getContestantsPoetryGenreZtoA);
+api.get('/getContestantsDeclamationDateUpward', contestantController.getContestantsDeclamationDateUpward);
+api.get('/getContestantsDeclamationDateDescendant', contestantController.getContestantsDeclamationDateDescendant);
+api.get('/getContestantsFullNameAtoZ', contestantController.getContestantsFullNameAtoZ);
+api.get('/getContestantsFullNameZtoA', contestantController.getContestantsFullNameZtoA);
 api.get('/getContestant/:id', contestantController.getContestant);
 
 module.exports = api;
